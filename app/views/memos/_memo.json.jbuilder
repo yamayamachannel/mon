d0b8cea053832c4d94b9e630fd94b340645b26bc
Yuji <q111014y@mail.kyutech.jp>
2019-10-08 18:30:30 +0900

    firstcom

---
+++ app/views/memos/_memo.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! memo, :id, :title, :bank, :wallet, :created_at, :updated_at
+json.url memo_url(memo, format: :json)
---
+++ db/migrate/20191008085936_create_memos.rb
@@ -0,0 +1,11 @@
+class CreateMemos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :memos do |t|
+      t.string :title
+      t.integer :bank
+      t.integer :wallet
+
+      t.timestamps
+    end
+  end
+end
